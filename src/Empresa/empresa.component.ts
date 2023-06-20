b7f067f7dc1e85fed9197116b74e23b27b0c7649
THIAGO ALESSANDRO BATISTA <thiago_a_batista@estudante.sesisenai.org.br>
2023-06-20 20:40:23 -0300

    versao 6

--- src/Empresa/empresa.component.ts
+++ src/Empresa/empresa.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 
 interface Cardapio{
-    empresa: Empresa|string
-    listaItens:Item[]
+    nomeEmpresa:string
+    itensCardapio:Item[]
+    categoria:string
 }
 interface Pedido {
     itens: Item[];
-    quantidade: number;
+    endereco:string
+    nomeEmpresa:string
+    //ta faltando a data ai
 }
+
+
 interface Item {
     nomeItem:string;
     precoItem:number;
@@ -21,6 +26,7 @@ interface Empresa{
     numeroContaBancaria:string,
     agencia:string,
     nomeDoResponsavel:string
+    cardapio?:Cardapio
 }
 
 @Component({
@@ -29,16 +35,70 @@ interface Empresa{
     //styleUrls: ["./empresa.component.css"]
 })
 
-
-export class EmpresaComponent{
-
 export class EmpresaComponent implements OnInit{
     logoLaranja:string;
+
+    empresaLogada:Empresa
+
     exibindoCardapio:boolean
 
     cardapiosLista:Cardapio[]
+    itensCardapio:Item[]
     pedidosLista:Pedido[]
+
+    novoItemNome:string
+    novoItemPreco:number
 
     ngOnInit(): void {
         this.logoLaranja = "./assets/imagens/logoLaranja.png"
+
+        let cardapios = JSON.parse(localStorage.getItem('cardapios'))
+        if(cardapios){
+            this.cardapiosLista = cardapios
+
+            if(JSON.parse(localStorage.getItem('logado'))){
+                this.empresaLogada = JSON.parse(localStorage.getItem('logado'))
+            
+                this.empresaLogada.cardapio = this.recuperarCardapio()
+            
+                this.itensCardapio = this.empresaLogada.cardapio.itensCardapio
+            }
+        }
+        this.pedidosLista = null // setar p n duplicar qnd roda dnv
+        let pedidos = JSON.parse(localStorage.getItem('pedidos'))
+        if(pedidos){//verifica se n ta nulo
+            for(let pedido of pedidos){//passa por cada um
+                if(pedido.nomeEmpresa == this.empresaLogada.nome){//verifica se o pedido é dessa empresa logada msm
+                    this.pedidosLista.push(pedido)  //adiciona na lista
+                }
+            }
+        }
+
+        // this.logado = JSON.parse(localStorage.getItem('logado'))
+
+        // this.logado.cardapio = this.recuperarCardapio()
+        
+        // this.itensCardapio = this.logado.cardapio.itensCardapio
     }
+    recuperarCardapio():Cardapio{
+        for(let cardapio of this.cardapiosLista){
+            if(cardapio.nomeEmpresa == this.empresaLogada.nome){
+                return cardapio
+            }
+        }
+        return null
+    }
+
+    alterarExibicaoCardapio(){
+        this.exibindoCardapio = !this.exibindoCardapio
+    }
+
+    adicionarNovoItemAoCardapio(){
+        let novoItem:Item = {
+            nomeItem: this.novoItemNome,
+            precoItem: this.novoItemPreco
+        }
+        this.itensCardapio.push(novoItem)
+    }
+    
+}
--- src/Empresa/empresa.module.ts
+++ src/Empresa/empresa.module.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { EmpresaComponent } from "./empresa.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
     declarations:[
         EmpresaComponent
     ],
     imports:[
-        CommonModule
+        CommonModule,
+        FormsModule
+        
     ]
 })
 
